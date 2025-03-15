d6fe1382736759770ca836c1023afa7b04fedd87
Ferry Agus Pratama <ferryaguspratama@gmail.com>
2025-03-15 09:55:21 +0700

    chore: mapping errors to arrays to improve readability of responses

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,10 +2,16 @@ import { NextFunction, Request, Response } from "express";
 
 export class ApiError extends Error {
     statusCode: number;
+    errors?: Record<string, string[]>;
 
-    constructor(statusCode: number, message: string) {
+    constructor(
+        statusCode: number,
+        message: string,
+        errors?: Record<string, string[]>
+    ) {
         super(message);
         this.statusCode = statusCode;
+        this.errors = errors;
     }
 }
 
@@ -15,10 +21,11 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
 ) => {
-    console.error(err);
-
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
+    const errors = err.errors || null;
 
-    return res.status(statusCode).json({ message });
+    console.error({ message, errors });
+
+    return res.status(statusCode).json({ success: false, message, errors });
 };
